11e4703ce2abb566f4c3fa42c9cb064d97f4bbf9
Ahmed <ahmedsalama70000@gmail.com>
2026-06-30 17:19:22 +0300

    fix case in profile page

--- frontend/src/web/profile/Replies.jsx
+++ frontend/src/web/profile/Replies.jsx
@@ -28,6 +28,14 @@ export default function Replies() {
       </div>
     );
   }
+
+  if (!userComments) {
+    return (
+      <div className="flex justify-center items-center h-40 text-gray-500">
+        لا يوجد ردود حتى الآن
+      </div>
+    );
+  }
   return (
     <section className="space-y-4">
       {userComments?.map((comment) => (
